ef15e8d6b136d95cb23b8481684d58cef8dad732
mubashir <hmubashir082gmail.com>
2020-06-09 18:50:29 +0530

    background images swapped

--- script/script.js
+++ script/script.js
@@ -3,20 +3,20 @@ $(window).scroll( function() {
     var product = $('.product').offset();
     var window = $(this).scrollTop();
     if ( window > 2000 && window < product.top) {
-        $('body').css('background-image','url("images/services/SprayBG2.jpg")');
+        $('body').css('background-image','url("images/services/CrewBG3.jpg")');
     } 
     else if( window > product.top) {
         $('body').css('background-image','url("images/back.png")');
     }
     else if( window < 2000) {
-        $('body').css('background-image','url("images/services/CrewBG3.jpg")');
+        $('body').css('background-image','url("images/services/SprayBG2.jpg")');
     }
 });
 
 
 $(document).read( function() {
     var clientSwiper = new Swiper('.swiper-container', {
-        slidesPerView: 3,
+        slidesPerView: 6,
         spaceBetween: 30,
         centeredSlides: true,
         loop: true,
