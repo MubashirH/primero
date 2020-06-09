a474879844e176f1eda4b6ad1d1e4fb4b1cb79a2
mubashir <hmubashir082gmail.com>
2020-06-09 18:00:49 +0530

    About section added

---
+++ script/script.js
@@ -0,0 +1,29 @@
+$(window).scroll( function() {
+    var white = $('.white');
+    var product = $('.product').offset();
+    var window = $(this).scrollTop();
+    if ( window > 2000 && window < product.top) {
+        $('body').css('background-image','url("images/services/SprayBG2.jpg")');
+    } 
+    else if( window > product.top) {
+        $('body').css('background-image','url("images/back.png")');
+    }
+    else if( window < 2000) {
+        $('body').css('background-image','url("images/services/CrewBG3.jpg")');
+    }
+});
+
+
+$(document).read( function() {
+    var clientSwiper = new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        centeredSlides: true,
+        loop: true,
+        grabCursor: true,
+        autoPlay: {
+            delay: 500,
+            disableOnInteraction: true
+        }
+    })
+});
